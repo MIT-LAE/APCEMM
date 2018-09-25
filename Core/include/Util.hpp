8f4e35ced2e8aba9cbecbdc2088c0dad06e8d42c
Thibaud Fritz <fritzt@mit.edu>
2018-09-25 18:49:53 -0400

    Updated Util structure to ease conversion from 2D vector to array

--- Core/include/Util.hpp
+++ Core/include/Util.hpp
@@ -7,10 +7,12 @@
 /*                                                                  */
 /* Author               : Thibaud M. Fritz                          */
 /* Time                 : 7/26/2018                                 */
-/* File                 : Util.h                                    */
-/* Working directory    : /home/fritzt/APCEMM-SourceCode            */
+/* File                 : Util.hpp                                  */
 /*                                                                  */
 /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+#ifndef UTIL_H_INCLUDED
+#define UTIL_H_INCLUDED
 
 #include <iostream>
 #include <vector>
@@ -18,8 +20,17 @@
 namespace util
 {
     double** EW_Multiply( double** A, double** B, unsigned int N, unsigned int M );
-    double** Vect2Array( std::vector<std::vector<double> > &vals, unsigned int N, unsigned int M );
-    std::vector<std::vector<double> > Array2Vect( double** A, unsigned int N, unsigned int M);
+    double* vect2double ( std::vector<std::vector<double> > &vals, unsigned int N, unsigned int M, double scalingFactor = 1.0 );
+    double* vect2double ( std::vector<double> &vals, unsigned int N, double scalingFactor = 1.0 );
+    float* vect2float ( std::vector<std::vector<double> > &vals, unsigned int N, unsigned int M, double scalingFactor = 1.0 );
+    float* vect2float ( std::vector<double> &vals, unsigned int N, double scalingFactor = 1.0 );
+    std::vector<std::vector<double> > Array2Vect( double** A, unsigned int N, unsigned int M, double scalingFactor = 1.0 );
     void PrintVector( std::vector<std::vector<double> > Array );
+    template <class T>
+    void delete1D( T* temp );
+    template <class T>
+    void delete2D( T** temp, unsigned int N );
 
 }
+
+#endif /* UTIL_H_INCLUDED */
