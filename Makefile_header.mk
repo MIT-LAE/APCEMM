e1d636700f43471bfe427f1873097d05ba743000
Thibaud Fritz <fritzt@mit.edu>
2018-10-04 13:15:03 -0400

    Added makefile header

---
+++ Makefile_header.mk
@@ -0,0 +1,243 @@
+#------------------------------------------------------------------------------
+#                 MIT Laboratory for Aviation and the Environment
+#------------------------------------------------------------------------------
+#      							C++ - APCEMM                                  #
+#      A(ircraft) P(lume) C(hemistry) E(mission) M(icrophysics) M(odel)       #
+#------------------------------------------------------------------------------
+#
+# !IROUTINE: Makefile_header.mk
+#
+# !DESCRIPTION: This sub-makefile defines the variables which specify 
+# compilation options for the different support compiler/platform combinations.
+# Also, the default makefile compilation rules are specified here.
+#
+# NOTE: We now use SHELL :=/bin/bash as the default Unix shell.
+#
+# !REVISION HISTORY:
+# 03 Oct 2018 - T. Fritz - Initial version
+#------------------------------------------------------------------------------
+
+###############################################################################
+###                                                                         ###
+###  Set the default Unix shell and some error message variables            ###
+###                                                                         ###
+###############################################################################
+
+# Set default shell to bash, for use with the Makefile conditionals
+SHELL                :=/bin/bash
+
+# Error message for bad COMPILER input
+ERR_CMPLR            :="Select a compiler: COMPILER=g++, COMPILER=gcc"
+
+# Error message for unknown compiler/OS combintation
+ERR_OSCOMP           :="Makefile_header.mk not set up for this compiler/OS combination"
+
+###############################################################################
+###                                                                         ###
+###  Set C-preprocessor switches representing user options.  These are not  ###
+###  specific to any compiler, but are general options for the simulation.  ###
+###                                                                         ###
+###############################################################################
+
+#------------------------------------------------------------------------------
+# Compiler settings
+#------------------------------------------------------------------------------
+
+# %%%%% OpenMP parallelization (on by default) %%%%%
+ifndef OMP
+  OMP                :=no
+endif
+
+# Option to turn off OpenMP for testing
+REGEXP               :=(^[Nn]|^[Oo])
+ifeq ($(shell [[ "$(OMP)" =~ $(REGEXP) ]] && echo true),true)
+  USER_DEFS          += -DNO_OMP
+endif
+
+# %%%%% Set default compiler %%%%%
+
+# %%%%% If COMPILER is not defined, default to the $(CXX) variable, which %%%%%
+# %%%%% is set in your .bashrc, or when you load the compiler module      %%%%%
+ifndef COMPILER
+  COMPILER           :=$(CXX)
+endif
+
+# %%%%% Test if g++ is selected %%%%%
+REGEXP               :=(^[Gg][+][+]|[Cc][Xx][Xx])
+ifeq ($(shell [[ "$(COMPILER)" =~ $(REGEXP) ]] && echo true),true)
+  COMPILE_CMD        :=$(CXX)
+  USER_DEFS          += -DLINUX_CXX
+endif
+
+# %%%%% Test if g++ is selected %%%%%
+REGEXP               :=(^[Gg][Cc][Cc]|^[Cc][Cc])
+ifeq ($(shell [[ "$(COMPILER)" =~ $(REGEXP) ]] && echo true),true)
+  COMPILE_CMD        :=$(CC)
+  USER_DEFS          += -DLINUX_CC
+endif
+
+# %%%%% ERROR CHECK!  Make sure our COMPILER selection is valid! %%%%%
+REGEXP               :=((-DLINUX_)?|CXX|CC)
+ifneq ($(shell [[ "$(USER_DEFS)" =~ $(REGEXP) ]] && echo true),true)
+  $(error $(ERR_CMPLR))
+endif
+
+#------------------------------------------------------------------------------
+# Diagnostic settings
+#------------------------------------------------------------------------------
+
+# %%%%% NETCDF (for using new netCDF diagnostic output) %%%%%
+REGEXP               :=(^[Yy]|^[Yy][Ee][Ss])
+ifeq ($(shell [[ "$(NC_DIAG)" =~ $(REGEXP) ]] && echo true),true)
+  USER_DEFS          += -DNC_DIAG
+  IS_NC_DIAG         :=1
+endif
+
+#------------------------------------------------------------------------------
+# Add test for mass conservation
+#------------------------------------------------------------------------------
+
+REGEXP               :=(^[Yy]|^[Yy][Ee][Ss])
+ifeq ($(shell [[ "$(MASSCONS)" =~ $(REGEXP) ]] && echo true),true)
+  USER_DEFS          += -DMASSCONS
+endif
+
+###############################################################################
+###                                                                         ###
+###  Set linker commands for local and external libraries (incl. netCDF)    ###
+###                                                                         ###
+###############################################################################
+
+# netCDF Library include path.  
+
+# Get the version number (e.g. "4130"=netCDF 4.1.3; "4200"=netCDF 4.2, etc.)
+NC_VERSION := $(shell nc-config --version)
+NC_VERSION := $(shell echo "$(NC_VERSION)" | sed 's|netCDF ||g')
+NC_VERSION := $(shell echo "$(NC_VERSION)" | sed 's|\.||g')
+NC_VERSION_LEN       :=$(shell perl -e "print length $(NC_VERSION)")
+
+ifeq ($(NC_VERSION_LEN),3)
+ NC_VERSION          :=$(NC_VERSION)0
+endif
+ifeq ($(NC_VERSION_LEN),2) 
+ NC_VERSION          :=$(NC_VERSION)00
+endif
+
+# Add path/to/lib folder
+LINK := $(LINK)
+
+# Add path to APCEMM's library folder
+LINK := -L$(LIB_DIR)
+
+# Define any libraries to link into executable: use -llibname option
+LINK := $(LINK) -lstdc++ -lm
+
+# Create linker command to create the APCEMM executable
+LINK := $(LINK) -Wl,--start-group -lUtil -lSands -lfftw3 -lKpp -lEpm -lAim -lnetcdf_c++ -Wl,--end-group
+
+###############################################################################
+###                                                                         ###
+###  Define settings for the GNU C++ COMPILER (aka g++)                     ###
+###                                                                         ###
+###############################################################################
+
+ifeq ($(COMPILER),g++) 
+
+  # Get the GNU Fortran version
+  VERSIONTEXT        :=$(shell $(CXX) --version)
+  VERSION            :=$(word 4, $(VERSIONTEXT))
+  VERSION            :=$(subst .,,$(VERSION))
+
+  # Base set of compiler flags
+  CXXLAGS            :=-cpp -std=c++11 -w -std=legacy -fautomatic -fno-align-commons
+  CXXLAGS            += -fconvert=big-endian
+  CXXLAGS            += -fno-range-check
+
+  # Default optimization level for all routines (-O3)
+  ifndef OPT
+    # Options of interest
+    #  -limf                Intel math libraries - machine must have them
+    #  -O3                  Highest safe optimization level
+    #  -march=native        Make the binary machine-specific. If in doubt, 
+    #                        use a specific architecture, eg...
+    #  -march=corei7-avx    Binary uses optimizations for 
+    #                        Intel Sandy-Bridge Xeon (e.g. E5-2680)
+    #  -mfpmath=sse         Use SSE extensions
+    #  -funroll-loops       Enable loop unrolling
+    OPT              := -O3 -funroll-loops
+  endif
+  
+  # Pick compiler options for debug run or regular run 
+  REGEXP             := (^[Yy]|^[Yy][Ee][Ss])
+  ifeq ($(shell [[ "$(DEBUG)" =~ $(REGEXP) ]] && echo true),true)
+    #-fcheck=all would be more comprehensive but would force bounds checking
+    CXXLAGS          += -Wall -Wextra -Wconversion
+    CXXLAGS          += -Warray-temporaries -fcheck-array-temporaries
+    USER_DEFS        += -DDEBUG
+  else
+    CXXLAGS          += $(OPT)
+  endif
+  
+  # Turn on OpenMP parallelization
+  REGEXP             :=(^[Yy]|^[Yy][Ee][Ss])
+  ifeq ($(shell [[ "$(OMP)" =~ $(REGEXP) ]] && echo true),true)
+    CXXLAGS           += -fopenmp
+  endif
+  
+  # Append the user options in USER_DEFS to CXXFLAGS
+  CXXFLAGS            += $(USER_DEFS)
+
+#  CFLAGS              := CXXFLAGS
+
+  CFLAGS := -Wall -Wextra -O3 -std=c++11
+
+  # Include options (i.e. for finding *.h* files)
+  INCLUDE := -I$(ROOT_DIR)/include
+
+endif
+
+# Set the standard compiler variables
+GCC  := $(COMPILE_CMD) $(CFLAGS) $(INCLUDE) $(LINK)
+LD   := $(COMPILE_CMD) $(CFLAGS)
+
+###############################################################################
+###                                                                         ###
+###  Specify pattern rules for compiliation                                 ###
+###  (i.e. tell "make" how to compile files w/ different extensions)        ###
+###                                                                         ###
+###############################################################################
+
+%.o : %.cpp
+	$(GCC) -o $@ -c $< 
+%.o : %.c
+	$(GCC) -o $@ -c $<
+
+###############################################################################
+###                                                                         ###
+###  Export global variables so that the main Makefile will see these       ###
+###                                                                         ###
+###############################################################################
+
+export GCC
+export LD
+export INCLUDE
+export LINK
+export SHELL
+
+###############################################################################
+###                                                                         ###
+###  Debug print output.  Normally you will leave the following lines       ###
+###  commented out.  Uncomment these lines for testing.                     ###
+###                                                                         ###
+###############################################################################
+
+#headerinfo:
+#	@@echo '####### in Makefile_header.mk ########'
+#	@@echo "COMPILER    : $(COMPILER)"
+#	@@echo "DEBUG       : $(DEBUG)"
+#	@@echo "CXX         : $(CXX)"
+#	@@echo "CC          : $(CC)"
+#	@@echo "INCLUDE     : $(INCLUDE)"
+#	@@echo "LINK        : $(LINK)"
+#	@@echo "USERDEFS    : $(USER_DEFS)"
+
