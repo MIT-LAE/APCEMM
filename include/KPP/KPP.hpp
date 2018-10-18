164da783971ed8af6cdd1e5dea18d301a0ded80e
Thibaud Fritz <fritzt@mit.edu>
2018-10-18 19:19:33 -0400

    Header file for C to C++

--- include/KPP/KPP.hpp
+++ include/KPP/KPP.hpp
@@ -15,13 +15,22 @@
 #ifndef KPP_H_INCLUDED
 #define KPP_H_INCLUDED
 
+#include "KPP/KPP_Parameters.h"
+
 #ifdef __cplusplus
 extern "C" {
 #endif /* __cplusplus */
+
 int KPP_Main( double varArray[], double fixArray[], double currentT, double dt, \
-              double airDens, double temperature, double pressure, \
-              double sinLAT, double cosLAT, double sinDEC, double cosDEC, \
               double rtols, double atols );
+void Update_RCONST( const double TEMP, const double PRESS,  \
+                    const double AIRDENS, const double H2O );
+void GC_SETHET( const double TEMP, const double PATM, const double AIRDENS, \
+                const double RELHUM, const unsigned int STATE_PSC,          \
+                const double SPC[], const double AREA[NAERO],               \
+                const double RADI[NAERO], const double IWC,                 \
+                const double KHETI_SLA[11] );
+void Read_JRates ( double JRates[], const double CSZA );
 #ifdef __cplusplus
 }
 #endif /* __cplusplus */
