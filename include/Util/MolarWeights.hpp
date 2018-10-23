f229a3a142346f9a41721ef57893ad4fd6f53f5e
Thibaud Fritz <fritzt@mit.edu>
2018-10-23 11:33:03 -0400

    Added molar weights

--- include/Util/MolarWeights.hpp
+++ include/Util/MolarWeights.hpp
@@ -27,19 +27,27 @@
 #define MW_H2O    18.0153E-03
 #define MW_HNO3   63.0128E-03
 #define MW_S      32.0655E-03
-#define MW_SO4    98.0785E-03
+#define MW_H2SO4  98.0785E-03
+#define MW_SO4    96.0785E-03
 #define MW_SO2    64.0638E-03
 #define MW_NO     30.0061E-03
 #define MW_NO2    46.0055E-03
 #define MW_HNO2   47.0134E-03
 #define MW_C2H6   30.0690E-03
+#define MW_N2O5   108.010E-03
 #define MW_PRPE   42.0800E-03
 #define MW_ALK4   58.1230E-03
 #define MW_CH2O   30.0260E-03
 #define MW_ALD2   44.0530E-03
 #define MW_GLYX   58.0370E-03
 #define MW_MGLY   72.0640E-03
-
-
+#define MW_NIT    62.0000E-03
+#define MW_NAT    117.000E-03
+#define MW_BrNO3  141.909E-03
+#define MW_ClNO3  97.4579E-03
+#define MW_HOCl   52.4603E-03
+#define MW_HCl    36.4580E-03
+#define MW_HOBr   96.9110E-03
+#define MW_HBr    80.9119E-03
 
 #endif /* MOLARWEIGHTS_H_INCLUDED */
