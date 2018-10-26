3b8c0178bbb673ef84891d4e1139113fb4bd5624
Thibaud Fritz <fritzt@mit.edu>
2018-10-26 10:38:24 -0400

    File merging + Rename

--- src/Core/Advection.cpp
+++ src/Core/Advection.cpp
@@ -3,11 +3,11 @@
 /*     Aircraft Plume Chemistry, Emission and Microphysics Model    */
 /*                             (APCEMM)                             */
 /*                                                                  */
-/* AdvGlobal Program File                                           */
+/* Advection Program File                                           */
 /*                                                                  */
 /* Author               : Thibaud M. Fritz                          */
 /* Time                 : 7/26/2018                                 */
-/* File                 : AdvGlobal.cpp                             */
+/* File                 : Advection.cpp                             */
 /*                                                                  */
 /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
 
@@ -71,3 +71,26 @@ void AdvGlobal( double time, double &v_x, double &v_y, double &dTrav_x, double &
     }
 
 } /* End of AdvGlobal */
+
+void AdvParam( double time, double &v_x, double &v_y )
+{
+
+    /* AdvParam:
+     * Computes advection parameters
+     *
+     * INPUTS:
+     * (double) time: current time since simulation started in [s]
+     *
+     * OUTPUTS:
+     * (double) v_x: current horizontal velocity at time in [m/s]
+     * (double) v_y: current vertical velocity at time in [m/s]
+     */
+
+    v_x = 0.0;
+
+    v_y = 0.0;
+
+
+} /* End of AdvParam */
+
+/* End of Advection.cpp */
