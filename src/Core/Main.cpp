9742d72350a179538513288a21fd709d05d88dea
Thibaud Fritz <fritzt@mit.edu>
2018-11-09 18:54:51 -0500

    Rearranged main file to be able to handle parallel simulations

--- src/Core/Main.cpp
+++ src/Core/Main.cpp
@@ -27,108 +27,127 @@ int PlumeModel( const unsigned int iCase,                   \
 
 int main( int , char* [] )
 {
-
-    const bool doPrint = false;
+            
     std::vector<std::vector<double> > parameters;
-    unsigned int nCases;
+    unsigned int iCase, nCases;
 
-    /* Print welcome message */
-    PrintMessage( doPrint );
-
-    /* Read in parameters */
-    parameters = ReadParameters();
-
-    /* Number of cases */
-    nCases  = parameters[0].size();
-
-    std::cout << "\nRunning model for " << nCases << " case(s) on " << omp_get_num_procs() << " procs\n";
-
-#pragma omp parallel for schedule(dynamic, 1)
-    for ( unsigned int i = 0; i < nCases; i++ ) {
-
-#pragma omp critical
+    #pragma omp master
         {
-            std::cout << "-> Running case " << i << " on thread " << omp_get_thread_num() << "\n";
+        
+            const bool doPrint = false;
+
+            /* Print welcome message */
+            PrintMessage( doPrint );
+
+            /* Read in parameters */
+            parameters = ReadParameters();
+
+            /* Number of cases */
+            nCases  = parameters[0].size();
+
         }
 
-        double temperature_K = parameters[0][i];
-        double pressure_Pa   = parameters[1][i];
-        double relHumidity_w = parameters[2][i];
-        double longitude_deg = parameters[3][i];
-        double latitude_deg  = parameters[4][i];
+    /* Synchronize the threads */
+    #pragma omp barrier
 
-        const unsigned int model = 1;
+    #pragma omp single
+        {
 
-        /*
-         * model = 0 -> Box Model
-         *
-         * model = 1 -> Plume Model
-         *                   + 
-         *              Adjoint Model
-         *
-         * model = 2 -> Adjoint Model
-         *
-         * model = 3 -> Box Model
-         *                  +
-         *              Plume Model
-         */
+            std::cout << "\n Running model for " << nCases << " case(s)\n"; 
+            std::cout << " Number of processors: " << omp_get_num_procs() << "\n";
+            std::cout << " Number of threads   : " << omp_get_max_threads() << "\n";
 
-        int iERR = 0;
-
-        switch (model) {
-
-            /* Box Model */
-            case 0:
-
-                std::cout << "Not implemented yet\n";
-                break;
-
-            /* Plume Model (APCEMM) */
-            case 1:
-
-                iERR = PlumeModel( i, temperature_K, pressure_Pa, \
-                                   relHumidity_w, longitude_deg,  \
-                                   latitude_deg );
-
-                break;
-
-            /* Adjoint Model */
-            case 2:
-
-                std::cout << "Not implemented yet\n";
-                break;
-
-            case 3:
-
-                std::cout << "Not implemented yet\n";
-                break;
-
-            default:
-
-                std::cout << "Wrong input for model\n";
-                std::cout << "model = " << model << "\n";
-                std::cout << "Value should be between 0 and 3\n";
-                break;
-                
         }
 
-        if ( iERR < 0 ) {
-            std::cout.precision(3);
-            std::cout << "\nAPCEMM Case: " << i << " failed on thread " << omp_get_thread_num() << ".\n";
-            std::cout << "Error: " << iERR << "\n";
-            std::cout << std::fixed;
-            std::cout << std::setprecision(3);
-            std::cout << "T   : " << std::setw(8) << temperature_K << " [K]\n";
-            std::cout << "P   : " << std::setw(8) << pressure_Pa/((double) 100.0) << " [hPa]\n";
-            std::cout << "RH_w: " << std::setw(8) << relHumidity_w << " [%]\n";
-            std::cout << "LON : " << std::setw(8) << longitude_deg << " [deg]\n";
-            std::cout << "LAT : " << std::setw(8) << latitude_deg << " [deg]\n";
-        }
-        else {
-            std::cout << "APCEMM Case: " << i << " completed.\n";
-        }
+    #pragma omp parallel for schedule(dynamic, 1) shared(parameters,nCases)
+        for ( iCase = 0; iCase < nCases; iCase++ ) {
 
-    }
+            double temperature_K = parameters[0][iCase];
+            double pressure_Pa   = parameters[1][iCase];
+            double relHumidity_w = parameters[2][iCase];
+            double longitude_deg = parameters[3][iCase];
+            double latitude_deg  = parameters[4][iCase];
+
+        #pragma omp critical
+            { std::cout << "-> Running case " << iCase << " on thread " << omp_get_thread_num() << "\n"; }
+
+
+            const unsigned int model = 1;
+
+            /*
+             * model = 0 -> Box Model
+             *
+             * model = 1 -> Plume Model
+             *                   + 
+             *              Adjoint Model
+             *
+             * model = 2 -> Adjoint Model
+             *
+             * model = 3 -> Box Model
+             *                  +
+             *              Plume Model
+             */
+
+            int iERR = 0;
+
+            switch (model) {
+
+                /* Box Model */
+                case 0:
+
+                    std::cout << "Not implemented yet\n";
+                    break;
+
+                /* Plume Model (APCEMM) */
+                case 1:
+
+                    iERR = PlumeModel( iCase, temperature_K, pressure_Pa, \
+                                       relHumidity_w, longitude_deg,      \
+                                       latitude_deg );
+                    break;
+
+                /* Adjoint Model */
+                case 2:
+
+                    std::cout << "Not implemented yet\n";
+                    break;
+
+                case 3:
+
+                    std::cout << "Not implemented yet\n";
+                    break;
+
+                default:
+
+                    std::cout << "Wrong input for model\n";
+                    std::cout << "model = " << model << "\n";
+                    std::cout << "Value should be between 0 and 3\n";
+                    break;
+                    
+            }
+
+            #pragma omp critical 
+            {
+
+                if ( iERR < 0 ) {
+                    std::cout.precision(3);
+                    std::cout << "\n APCEMM Case: " << iCase << " failed on thread " << omp_get_thread_num() << ".\n";
+                    std::cout << " Error: " << iERR << "\n";
+                    std::cout << std::fixed;
+                    std::cout << std::setprecision(3);
+                    std::cout << " T   : " << std::setw(8) << temperature_K << " [K]\n";
+                    std::cout << " P   : " << std::setw(8) << pressure_Pa/((double) 100.0) << " [hPa]\n";
+                    std::cout << " RH_w: " << std::setw(8) << relHumidity_w << " [%]\n";
+                    std::cout << " LON : " << std::setw(8) << longitude_deg << " [deg]\n";
+                    std::cout << " LAT : " << std::setw(8) << latitude_deg << " [deg]\n";
+                }
+                else {
+                    std::cout << " APCEMM Case: " << iCase << " completed.\n";
+                }
+
+            }
+
+        }
 
     return 0;
 
