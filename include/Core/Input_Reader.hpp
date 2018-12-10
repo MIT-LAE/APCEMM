b6ac426842e767c0ca23a1122ab17c9587a6aba5
Thibaud Fritz <fritzt@mit.edu>
2018-12-10 11:04:43 -0500

    In Progress.. Input file reader

---
+++ include/Core/Input_Reader.hpp
@@ -0,0 +1,24 @@
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+/*                                                                  */
+/*     Aircraft Plume Chemistry, Emission and Microphysics Model    */
+/*                             (APCEMM)                             */
+/*                                                                  */
+/* Input_Reader Header File                                         */
+/*                                                                  */
+/* Author               : Thibaud M. Fritz                          */
+/* Time                 : 12/10/2018                                */
+/* File                 : Input_Reader.hpp                          */
+/*                                                                  */
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+#ifndef INPUT_READER_H_INCLUDED
+#define INPUT_READER_H_INCLUDED
+
+#include <iostream>
+#include <fstream>
+#include <cstring>
+
+void Read_Input_File( const char* fileName );
+
+#endif /* INPUT_READER_H_INCLUDED */
+
