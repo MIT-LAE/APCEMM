0f572d56a0c412bef578451c9a85caab1bcf3fba
Thibaud Fritz <fritzt@mit.edu>
2018-12-17 16:55:54 -0500

    Updated time step generation mechanism. Now smaller timesteps that the dynamic timesteps are possible (sunrise/sunset, ...). However, we also make sure that n*DYN_DT is always in the time array for all positive integer n

--- Code.v05-00/src/Core/BuildTime.cpp
+++ Code.v05-00/src/Core/BuildTime.cpp
@@ -14,11 +14,10 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
-#include "Core/Parameters.hpp"
 
 double UpdateTime( double time, const double tStart, \
                    const double sunRise, const double sunSet, \
-                   const double DYN_DT );
+                   const double DYN_DT, double& nextTimeStep );
 
 std::vector<double> BuildTime( const double tStart, const double tEnd, \
                                const double sunRise, const double sunSet, \
@@ -29,42 +28,53 @@ std::vector<double> BuildTime( const double tStart, const double tEnd, \
 
     std::vector<double> timeArray;
     double time = tStart;
-    double timeStep;
+    double timeStep, nextTimeStep;
+
+    timeStep = 0.0E+00;
+    nextTimeStep = 0.0E+00;
 
     while ( time < tEnd ) {
 
         /* For debug purposes, uncomment the next line */
-        //std::cout << time/3600 << ", " << timeStep << std::endl;
+        // std::cout << time/3600 << ", " << timeStep << "  ";
 
-        timeArray.push_back((double) 0.0);
-        timeArray[nT] = time;
-        timeStep = UpdateTime( time, tStart, sunRise, sunSet, DYN_DT );
+        timeArray.push_back( time );
+        timeStep = UpdateTime( time, tStart, sunRise, sunSet, \
+                               DYN_DT, nextTimeStep );
         time += std::min( timeStep, std::abs( ( tEnd - time ) ) );
         nT++;
     }
 
-    timeArray.push_back((double) 0.0);
-    timeArray[nT] = time;
-
+    timeArray.push_back( time );
     return timeArray;
 
 } /* End of BuildTime */
 
 double UpdateTime( double time, const double tStart, \
                    const double sunRise, const double sunSet, \
-                   const double DYN_DT )
+                   const double DYN_DT, double &nextTimeStep )
 {
 
     const double default_TimeStep = DYN_DT;
     double timeStep;
+    double currTimeStep;
+    bool sunCorrection = 0;
+
+    if ( nextTimeStep > 0.0E+00 ) {
+        timeStep = nextTimeStep;
+        nextTimeStep = 0.0E+00;
+        return timeStep;
+    }
 
     if (( time - tStart ) < 3600.0 && ( time - tStart ) >= 0.0) {
-        if (( time - tStart ) < 800.0)
-            timeStep = (double) 50.0;
-        else if (( time - tStart ) >= 800.0 && ( time - tStart) < 1200.0)
-            timeStep = (double) 100.0;
-        else if (( time - tStart ) >= 1200.0 && ( time - tStart) < 3600.0)
-            timeStep = (double) 300.0;
+        if ( ( time - tStart ) < default_TimeStep )
+            timeStep = default_TimeStep / (double) 10.0;
+        else if ( ( time - tStart ) >= default_TimeStep && \
+                  ( time - tStart ) < 2*default_TimeStep )
+            timeStep = default_TimeStep / (double) 5.0;
+        else if (( time - tStart ) >= 2*default_TimeStep && \
+                 ( time - tStart) < 6*default_TimeStep )
+            timeStep = default_TimeStep / (double) 2.0;
         else
             timeStep = default_TimeStep;
     }
@@ -72,12 +82,20 @@ double UpdateTime( double time, const double tStart, \
         timeStep = default_TimeStep;
 
     /* Add a small bit to ensure that we get passed the sunrise/set */
+    currTimeStep = timeStep;
     if ( (std::fmod((time),(24.0*3600.0)) < (sunSet)) \
-      && (std::fmod((time + timeStep),(24.0*3600.0)) > sunSet) )
+      && (std::fmod((time + timeStep),(24.0*3600.0)) > sunSet) ) {
         timeStep = sunSet - std::fmod((time),(24.0*3600.0)) + 1.00E-00;
+        sunCorrection = 1;
+    }
     if ( (std::fmod((time),(24.0*3600.0)) < (sunRise)) \
-      && (std::fmod((time + timeStep),(24.0*3600.0)) > sunRise) )
+      && (std::fmod((time + timeStep),(24.0*3600.0)) > sunRise) ) {
         timeStep = sunRise - std::fmod((time),(24.0*3600.0)) + 1.00E-00;
+        sunCorrection = 1;
+    }
+
+    if ( sunCorrection == 1 )
+        nextTimeStep = currTimeStep - timeStep;
 
     return timeStep;
 
