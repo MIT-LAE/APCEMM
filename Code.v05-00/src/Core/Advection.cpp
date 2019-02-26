8f6bb9962d567da245e313d2938ac8fcdf531429
Thibaud Fritz <fritzt@mit.edu>
2019-02-26 11:37:55 -0500

    Updated Advection routine to account for input options

--- Code.v05-00/src/Core/Advection.cpp
+++ Code.v05-00/src/Core/Advection.cpp
@@ -15,14 +15,19 @@
 #include <cmath>
 #include "Core/Parameters.hpp"
 
-void AdvGlobal( double time, double &v_x, double &v_y, double &dTrav_x, double &dTrav_y )
+void AdvGlobal( const double time, const double T_UPDRAFT, \
+                const double V_UPDRAFT,                    \
+                double &v_x, double &v_y,                  \
+                double &dTrav_x, double &dTrav_y )
 {
 
     /* AdvGlobal:
      * Computes domain advection parameters
      *
      * INPUTS:
-     * (double) time: current time since simulation started in [s]
+     * (double) time     : current time since simulation started in [s]
+     * (double) T_UPDRAFT: updraft timescale in [s]
+     * (double) V_UPDRAFT: initial upward velocity in [s]
      *
      * OUTPUTS:
      * (double) v_x: current horizontal velocity at time in [m/s]
@@ -35,39 +40,28 @@ void AdvGlobal( double time, double &v_x, double &v_y, double &dTrav_x, double &
     v_x = VX;
     dTrav_x = VX * time;
 
-    if ( SYNLIFT ) {
-       
-        if ( SYNPROF == 0 ) {
+    if ( SYNPROF == 0 ) {
 
-            /* Velocity step */
-            if ( time < T_SYN ) {
-                v_y = V_SYN;
-                dTrav_y = v_y * time;
-            }
-            else {
-                v_y = 0.0;
-            }
-
+        /* Velocity step */
+        if ( time < T_UPDRAFT ) {
+            v_y = V_UPDRAFT;
+            dTrav_y = v_y * time;
         }
-        else if ( SYNPROF == 1 ) {
+        else
+            v_y = 0.0;
 
-            /* Exponentially decaying velocity */
-            v_y = V_SYN * exp ( - time / T_SYN );
+    }
+    else if ( SYNPROF == 1 ) {
 
-            dTrav_y = V_SYN * T_SYN * ( 1.0 - exp ( - time / T_SYN ) );
+        /* Exponentially decaying velocity */
+        v_y = V_UPDRAFT * exp ( - time / T_UPDRAFT );
 
-        }
-        else {
-            std::string const currFile("AdvGlobal.cpp");
-            std::cout << "ERROR: In " << currFile << ":SYNPROF set to " << SYNPROF << std::endl;
-        }
+        dTrav_y = V_UPDRAFT * T_UPDRAFT * ( 1.0 - exp ( - time / T_UPDRAFT ) );
 
     }
     else {
-
-        v_y = VY;
-        dTrav_y = VY * time;
-
+        std::string const currFile("AdvGlobal.cpp");
+        std::cout << "ERROR: In " << currFile << ":SYNPROF set to " << SYNPROF << std::endl;
     }
 
 } /* End of AdvGlobal */
