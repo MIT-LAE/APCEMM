ef8e5e28f7d1e91604f0570f948f0cae4817f99c
Thibaud Fritz <fritzt@mit.edu>
2020-08-27 19:45:57 -0400

    Revert "Feat: Remove dependencies on Matlab libraries"
    This reverts commit f886de1ef0ddae1a5794288593b8d7eca1ed8d93.

--- Code.v05-00/Makefile_header.mk
+++ Code.v05-00/Makefile_header.mk
@@ -128,7 +128,7 @@ LINK := $(LINK)
 LINK := -L$(LIB_DIR)
 
 # Define any libraries to link into executable: use -llibname option
-LINK := $(LINK) $(LDLIBS) -lstdc++ -lmx -lmat
+LINK := $(LINK) $(LDLIBS) -lstdc++
 
 LINK_FFTW := -lfftw3 -lfftw3f -lfftw3l
 ifeq ($(shell [[ "$(OMP)" =~ $(REGEXP) ]] && echo true),true)
