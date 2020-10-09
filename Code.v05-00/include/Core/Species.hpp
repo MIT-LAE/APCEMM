1d3bf07460c4ae1e49a5c80ddda2d4851a197af5
Thibaud Fritz <fritzt@mit.edu>
2020-10-08 23:59:56 -0400

    Feat: Make APCEMM allow to accept different chemical mechanisms
    Signed-off-by: Thibaud Fritz <fritzt@mit.edu>

--- Code.v05-00/include/Core/Species.hpp
+++ Code.v05-00/include/Core/Species.hpp
@@ -61,28 +61,7 @@ class SpeciesArray
         bool gethalfRing() const { return halfRing; }
 
         /* Reactive species */
-        Vector_2D CO2, PPN, BrNO2, IEPOX, PMNN, N2O, N, PAN, ALK4, MAP, MPN,   \
-                  Cl2O2, ETP, HNO2, C3H8, RA3P, RB3P, OClO, ClNO2, ISOP, HNO4, \
-                  MAOP, MP, ClOO, RP, BrCl, PP, PRPN, SO4, Br2, ETHLN, MVKN,   \
-                  R4P, C2H6, RIP, VRP, ATOOH, IAP, DHMOB, MOBA, MRP, N2O5,     \
-                  ISNOHOO, ISNP, ISOPNB, IEPOXOO, MACRNO2, ROH, MOBAOO, DIBOO, \
-                  PMN, ISNOOB, INPN, H, BrNO3, PRPE, MVKOO, Cl2, ISOPND, HOBr, \
-                  A3O2, PROPNN, GLYX, MAOPO2, CH4, GAOO, B3O2, ACET, MACRN,    \
-                  CH2OO, MGLYOO, VRO2, MGLOO, MACROO, PO2, CH3CHOO, MAN2,      \
-                  ISNOOA, H2O2, PRN1, ETO2, KO2, RCO3, HC5OO, GLYC, ClNO3,     \
-                  RIO2, R4N1, HOCl, ATO2, HNO3, ISN1, MAO3, MRO2, INO2, HAC,   \
-                  HC5, MGLY, ISOPNBO2, ISOPNDO2, R4O2, R4N2, BrO, RCHO, MEK,   \
-                  ClO, MACR, SO2, MVK, ALD2, MCO3, CH2O, H2O, Br, NO, NO3, Cl, \
-                  O, O1D, O3, HO2, NO2, OH, HBr, HCl, CO, MO2;
-
-        /* Fixed species */
-        RealDouble ACTA, EOH, H2, HCOOH, MOH, N2, O2, RCOOH;
-        
-        /* Liquid species */
-        Vector_2D SO4L, H2OL, HNO3L, HClL, HOClL, HBrL, HOBrL;
-
-        /* Solid species */
-        Vector_2D H2OS, HNO3S;
+        Vector_3D Species;
 
         /* Aerosols */
         Vector_2D sootDens, sootRadi, sootArea, \
