7a499131b14649f2a2b35774ca2d5643a8f7915c
ca525 <ca525@cam.ac.uk>
2023-11-16 20:45:19 +0000

    Add the format for figure 2

--- examples/Example5_Sweep/sweep_plotter.m
+++ examples/Example5_Sweep/sweep_plotter.m
@@ -1,8 +1,8 @@
 clear all;
 close all;
 
-T_list = 217-10:1:217+20
-RH_list = 0:10:140
+T_list = 217-10:1:217+20;
+RH_list = 0:10:140;
 
 T_evals_matrix = readmatrix('APCEMM-sweep-evaluations-T.csv')
 T_evals = T_evals_matrix(2:end,end);
@@ -13,14 +13,28 @@ RH_evals = RH_evals_matrix(2:end,end);
 Times_matrix = readmatrix('APCEMM-sweep-times.csv')
 Times = Times_matrix(2:end,end);
 
+
+
+
 figure(1)
 % set figure position and size:
 set(gcf,'position',[160 200 500 400]) % 350
 
-semilogy(T_list, T_evals)
+%semilogy(T_list, T_evals)
+plot(T_list, T_evals)
+
+
+
 
 figure(2)
-semilogy(RH_list, RH_evals)
+% set figure position and size:
+set(gcf,'position',[160 200 500 400]) % 350
+
+%semilogy(RH_list, RH_evals)
+plot(RH_list, RH_evals)
+
+
+
 
 % hold on
 % scatter(x, y, 100, 'k',Marker='x',MarkerFaceColor='k')
