b8f1113fc788dc13d35c31490c9c99232a0b1b63
ca525 <ca525@cam.ac.uk>
2024-12-30 23:41:28 +0100

    Apply black formatting and change w units

--- scripts/simple_met_input/src/thermo.py
+++ scripts/simple_met_input/src/thermo.py
@@ -1,10 +1,11 @@
 import numpy as np
 
+
 ######################### WATER THERMODYNAMIC FUNCTIONS #########################
 def compute_p_sat_liq(T_K):
     # Calculates the liquid saturation pressure "p_sat_liq" (in units of Pa).
     # This equation can be found in Schumann 2012.
-    # 
+    #
     # The inputs are as follows:
     # - T_K is the absolute temperature in Kelvin
 
@@ -16,10 +17,11 @@ def compute_p_sat_liq(T_K):
 
     return 100 * np.exp(a / T_K + b + c * T_K + d * T_K * T_K + e * np.log(T_K))
 
+
 def compute_p_sat_ice(T_K):
     # Calculates the ice saturation pressure "p_sat_ice" (in units of Pa)
     # The equation can be found in Schumann 2012.
-    #  
+    #
     # The inputs are as follows
     # - T_K is the absolute temperature in Kelvin
 
@@ -31,50 +33,60 @@ def compute_p_sat_ice(T_K):
 
     return 100 * np.exp(a / T_K + b + c * T_K + d * T_K * T_K + e * np.log(T_K))
 
+
 def convert_RH_to_RHi(T_K, RH):
     return RH * compute_p_sat_liq(T_K) / compute_p_sat_ice(T_K)
 
+
 def convert_RHi_to_RH(T_K, RHi):
     return RHi * compute_p_sat_ice(T_K) / compute_p_sat_liq(T_K)
+
 
 def convert_RH_to_SH(T_K, p_Pa, RH_PC):
     # Implements the equations from https://earthscience.stackexchange.com/a/2361
     # The output specific humidity is in ratio format.
     # The input RH must be in %.
 
-    RH_ratio = RH_PC / 100.
-    R_d = 287 # J kg^-1 K^-1 from https://en.wikipedia.org/wiki/Gas_constant#:~:text=Specific%20gas%20constant,-Rspecific&text=for%20dry%20air%20of%2028.964917%20g%2Fmol.
-    R_v = 461.5 # J kg^-1 K^-1 from https://en.wikipedia.org/wiki/Water_vapor
+    RH_ratio = RH_PC / 100.0
+    R_d = 287  # J kg^-1 K^-1 from https://en.wikipedia.org/wiki/Gas_constant#:~:text=Specific%20gas%20constant,-Rspecific&text=for%20dry%20air%20of%2028.964917%20g%2Fmol.
+    R_v = 461.5  # J kg^-1 K^-1 from https://en.wikipedia.org/wiki/Water_vapor
 
-    e_s_Pa = compute_p_sat_liq(T_K) # Saturation pressure of water vapour
-    w_s = (R_d / R_v) * e_s_Pa / (p_Pa - e_s_Pa) # Mass mixing ratio of water vapor to dry air at equilibrium (dimensionless)
+    e_s_Pa = compute_p_sat_liq(T_K)  # Saturation pressure of water vapour
+    w_s = (
+        (R_d / R_v) * e_s_Pa / (p_Pa - e_s_Pa)
+    )  # Mass mixing ratio of water vapor to dry air at equilibrium (dimensionless)
 
-    w = RH_ratio * w_s # Mass mixing ratio of water vapor to dry air
+    w = RH_ratio * w_s  # Mass mixing ratio of water vapor to dry air
 
     return w / (w + 1)
+
 
 def convert_RHi_to_SH(T_K, p_Pa, RHi_PC):
     # Implements the equations from https://earthscience.stackexchange.com/a/2361
     # The output specific humidity is in ratio format.
     # The input RHi must be in %.
-    
+
     RH_PC = convert_RHi_to_RH(T_K, RHi_PC)
 
     return convert_RH_to_SH(T_K, p_Pa, RH_PC)
+
 
 def convert_SH_to_RH(T_K, p_Pa, SH):
     # Implements the equations from https://earthscience.stackexchange.com/a/2361
     # The specific humidity must be in ratio format. The output RH is in %
 
-    R_d = 287 # J kg^-1 K^-1 from https://en.wikipedia.org/wiki/Gas_constant#:~:text=Specific%20gas%20constant,-Rspecific&text=for%20dry%20air%20of%2028.964917%20g%2Fmol.
-    R_v = 461.5 # J kg^-1 K^-1 from https://en.wikipedia.org/wiki/Water_vapor
+    R_d = 287  # J kg^-1 K^-1 from https://en.wikipedia.org/wiki/Gas_constant#:~:text=Specific%20gas%20constant,-Rspecific&text=for%20dry%20air%20of%2028.964917%20g%2Fmol.
+    R_v = 461.5  # J kg^-1 K^-1 from https://en.wikipedia.org/wiki/Water_vapor
 
-    e_s_Pa = compute_p_sat_liq(T_K) # Saturation pressure of water vapour
-    w_s = (R_d / R_v) * e_s_Pa / (p_Pa - e_s_Pa) # Mass mixing ratio of water vapor to dry air at equilibrium (dimensionless)
+    e_s_Pa = compute_p_sat_liq(T_K)  # Saturation pressure of water vapour
+    w_s = (
+        (R_d / R_v) * e_s_Pa / (p_Pa - e_s_Pa)
+    )  # Mass mixing ratio of water vapor to dry air at equilibrium (dimensionless)
 
     w = SH / (1 - SH)
-    
+
     return w / w_s * 100
+
 
 def convert_SH_to_RHi(T_K, p_Pa, SH):
     # Implements the equations from https://earthscience.stackexchange.com/a/2361
