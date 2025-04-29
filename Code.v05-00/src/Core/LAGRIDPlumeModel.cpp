51b0a6513906d5460a34e5df078e3effeb438ece
Calebsakhtar <calebsakhtar@gmail.com>
2025-04-29 14:04:07 +0200

    Set all diff coeffs to 0

--- Code.v05-00/src/Core/LAGRIDPlumeModel.cpp
+++ Code.v05-00/src/Core/LAGRIDPlumeModel.cpp
@@ -437,6 +437,11 @@ void LAGRIDPlumeModel::runTransport(double timestep) {
         //Dont use enhanced diffusion on the H2O (and zero settling velocity)
         FVM_ANDS::FVM_Solver solver(fvmSolverInitParams, xCoords_, yCoords_, ZERO_BC_INIT, FVM_ANDS::std2dVec_to_eigenVec(H2O_));
         solver.updateTimestep(timestep);
+        if skipDiffusion {
+            solver.updateDiffusion(0, 0);
+        } else {
+            solver.updateDiffusion(input_.horizDiff(), input_.vertiDiff());
+        }
         solver.updateDiffusion(input_.horizDiff(), input_.vertiDiff());
         solver.updateAdvection(0, 0, shear_rep_);
 
@@ -465,7 +470,11 @@ void LAGRIDPlumeModel::runTransport(double timestep) {
         //Identical settings to H2O
         FVM_ANDS::FVM_Solver solver(fvmSolverInitParams, xCoords_, yCoords_, ZERO_BC_INIT, FVM_ANDS::std2dVec_to_eigenVec(Contrail_));
         solver.updateTimestep(timestep);
-        solver.updateDiffusion(input_.horizDiff(), input_.vertiDiff());
+        if skipDiffusion {
+            solver.updateDiffusion(0, 0);
+        } else {
+            solver.updateDiffusion(input_.horizDiff(), input_.vertiDiff());
+        }
         solver.updateAdvection(0, 0, shear_rep_);
 
         solver.operatorSplitSolve2DVec(Contrail_, ZERO_BC, skipDiffusion);
