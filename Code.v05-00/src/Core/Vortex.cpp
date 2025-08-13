2c0c8ee9bd960067e9c90595f0bd4efb7957bd0d
Calebsakhtar <calebsakhtar@gmail.com>
2025-08-13 17:10:18 +0100

    Add a message if the survival fraction is 0

--- Code.v05-00/src/Core/Vortex.cpp
+++ Code.v05-00/src/Core/Vortex.cpp
@@ -102,6 +102,9 @@ Vortex::Vortex( double RHi_PC, double temperature_K, double pressure_Pa,  \
     icenum_survfrac_ = beta_0_ + beta_1_ / physConst::PI * atan( alpha_0_ + z_delta_fns_ / 1.0E+02 );
     icenum_survfrac_ = std::min( std::max( icenum_survfrac_, 0.0E+00 ), 1.0E+00 );
 
+    if ( icenum_survfrac_ == 0.0E+00 )
+        std::cout << "Contrail has fully melted because of vortex-sinking losses" << std::endl;
+
     /* Compute the parametrized contrail height by repeating the calculations for the survival fraction
      * with gamma set to 0 as in U2016 */
     z_delta_h_ =+ alpha_atm_  * z_atm_  \
